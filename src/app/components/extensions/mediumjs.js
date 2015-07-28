eca1987fa871635a7945e90736e33227a18e9e64
aeneasr <aeneas.rekkas@serlo.org>
2015-07-28 22:45:48 +0200

    unstaged

--- src/app/components/extensions/mediumjs.js
+++ src/app/components/extensions/mediumjs.js
@@ -10,14 +10,34 @@ require('rangy/lib/rangy-classapplier.js');
 
 var React = require('react'),
     Medium = require('Medium.js'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    Animate = require('rc-animate');
 
 
 module.exports = React.createClass({
     getInitialState: function () {
         return {
-            instance: null
+            instance: null,
+            focus: false
         };
+    },
+    getDefaultProps: function () {
+        return {
+            actions: [
+                {
+                    invoke: 'strong',
+                    icon: 'fa fa-bold',
+                    options: {}
+                },
+                {
+                    invoke: 'em',
+                    icon: 'fa fa-italic',
+                    options: {}
+                }
+            ]
+        };
+    },
+    componentWillMount: function () {
     },
     componentDidMount: function () {
         var editable = React.findDOMNode(this.refs.editable);
@@ -28,14 +48,15 @@ module.exports = React.createClass({
                 attributes: null,
                 tags: null,
                 pasteAsText: false,
-                beforeInvokeElement: function(){
-                    console.log(this);
-                },
-                beforeInsertHtml: function () {
-                    console.log('before html');
-                },
-                beforeAddTag: function (tag, shouldFocus, isEditable, afterElement) {
-                    console.log('before tag');
+                keyContext: {
+                    'enter': function (e, element) {
+                        var sib = element.previousSibling;
+                        if (sib && sib.tagName == 'LI') {
+                            element.style.color = sib.style.color;
+                            element.className = sib.className;
+                            this.cursor.caretToBeginning(element);
+                        }
+                    }
                 }
             }),
             editable: editable
@@ -50,34 +71,66 @@ module.exports = React.createClass({
             if (!this.isInFocus()) {
                 this.state.instance.select();
             }
-            this.state.instance.invokeElement('strong', {});
+            this.state.instance.invokeElement(action.invoke, action.options);
         }.bind(this);
     },
+    listAction: function (outer) {
+        outer = outer === 'ul' ? outer : 'ol';
+        return function (event) {
+            var outer = document.createElement(outer || 'ul'),
+                inner = document.createElement('li');
+            event.preventDefault();
+            outer.appendChild(inner);
+            this.state.instance.focus();
+            this.state.instance.insertHtml(outer);
+        }.bind(this);
+    },
+    focus: function () {
+        this.setState({focus: true});
+    },
+    blur: function () {
+        this.setState({focus: false});
+    },
+    createButton: function (action, i) {
+        return (<button key={i}
+                        className="btn btn-primary"
+                        onMouseDown={this.toolbarAction(action)}>
+            <span className={action.icon}></span>
+        </button>);
+    },
+    createListButton: function (index, outer) {
+        var cn = 'fa fa-list' + (outer === 'ul' ? '' : '-ol');
+        return (<button key={index+1}
+                        className="btn btn-primary"
+                        onMouseDown={this.listAction(outer)}>
+            <span className={cn}></span>
+        </button>)
+    },
     render: function () {
-        var actions = [{
-            invoke: 'strong',
-            inner: 'B'
-        }, {
-            invoke: 'em',
-            inner: 'I'
-        }, {
-            invoke: 'u',
-            inner: 'U'
-        }], buttons = [];
-
-        _.each(actions, function (action, i) {
-            buttons.push(<button key={i} className="btn btn-primary" onMouseDown={this.toolbarAction(action)}>{action.inner}</button>)
+        var buttons = [];
+        _.each(this.props.actions, function (k, v) {
+            buttons.push(this.createButton(k, v));
         }.bind(this));
+        buttons.push(this.createListButton(buttons.length, 'ul'));
+        buttons.push(this.createListButton(buttons.length, 'ol'));
 
         return (
             /*jshint ignore:start */
-            <div>
-                <div className={this.state.instance ? '': ' hidden'}>
-                    <div class="btn-group">
-                        {buttons}
+            <div onFocus={this.focus} onBlur={this.blur}>
+                <Animate showProp="visible" transitionName="fade">
+                    <div visible={this.state.focus}
+                         style={{margin: '10px 0', display: this.state.focus ? 'block' : 'none'}}>
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                <div className="btn-group">
+                                    {buttons}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="editable-component" ref="editable"
+                </Animate>
+
+                <div className="editable-component" ref="editable" onFocus={focus}
                      dangerouslySetInnerHTML={{__html: this.props.content}}>
                 </div>
             </div>
