f2a85cbc1f73fd06d7183d6579b5839c855964b9
Aeneas Rekkas <aeneas.rekkas@serlo.org>
2015-07-29 11:35:22 +0200

    unstaged

--- src/app/components/toolbar.js
+++ src/app/components/toolbar.js
@@ -2,7 +2,8 @@
 
 var React = require('react'),
     Slideout = require('slideout'),
-    DragSource = require('react-dnd').DragSource;
+    DragSource = require('react-dnd').DragSource,
+    _ = require('underscore');
 
 module.exports = React.createClass({
     getInitialState: function () {
@@ -14,7 +15,42 @@ module.exports = React.createClass({
         return {
             padding: 256,
             tolerance: 70,
-            zIndex: 9000
+            zIndex: 9000,
+            sections: [
+                {
+                    title: 'Text',
+                    actions: [
+                        {innerHTML: '<span class="fa fa-th-large fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-th fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-columns fa-3x"></span>'}
+                    ]
+                },
+                {
+                    title: 'Media',
+                    actions: [
+                        {innerHTML: '<span class="fa fa-youtube-play fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-soundcloud fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-twitter fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-vimeo fa-3x"></span>'}
+                    ]
+                },
+                {
+                    title: 'Charts',
+                    actions: [
+                        {innerHTML: '<span class="fa fa-area-chart fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-pie-chart fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-bar-chart fa-3x"></span>'}
+                    ]
+                },
+                {
+                    title: 'Payment',
+                    actions: [
+                        {innerHTML: '<span class="fa fa-credit-card fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-paypal fa-3x"></span>'},
+                        {innerHTML: '<span class="fa fa-google-wallet fa-3x"></span>'}
+                    ]
+                }
+            ]
         };
     },
     componentDidMount: function () {
@@ -31,7 +67,29 @@ module.exports = React.createClass({
         });
         slideout.open();
     },
+    renderToolbarSections: function() {
+        var sections = [];
+        _.each(this.props.sections, function(group, i){
+            var actions = [];
+            _.each(group.actions, function(action, y) {
+                actions.push(<div key={y} className="col-xs-4 toolbar-section-action text-center" dangerouslySetInnerHTML={{__html: action.innerHTML}}></div>);
+            });
+
+            sections.push((
+                <section className="toolbar-section" key={i}>
+                    <div className="toolbar-section-header">{group.title}</div>
+                    <div className="row toolbar-section-group">
+                        {actions}
+                    </div>
+                </section>
+            ));
+
+        });
+        return sections;
+    },
     render: function () {
+
+
         return (
             /*jshint ignore:start */
             <div>
@@ -40,28 +98,7 @@ module.exports = React.createClass({
                         <header className="toolbar-header">
                             Pick a layout
                         </header>
-                        <section className="toolbar-section">
-                            <div className="toolbar-section-header">Plugin A</div>
-                            <div className="row toolbar-section-group">
-                                <div className="col-xs-5 toolbar-section-action">
-                                    <a href="#">Function 1</a>
-                                </div>
-                                <div className="col-xs-5 col-xs-offset-1 toolbar-section-action">
-                                    <a href="#">Function 1</a>
-                                </div>
-                            </div>
-                        </section>
-                        <section className="toolbar-section">
-                            <div className="toolbar-section-header">Plugin A</div>
-                            <div className="row toolbar-section-group">
-                                <div className="col-xs-5 toolbar-section-action">
-                                    <a href="#">Function 3</a>
-                                </div>
-                                <div className="col-xs-5 col-xs-offset-1 toolbar-section-action">
-                                    <a href="#">Function 4</a>
-                                </div>
-                            </div>
-                        </section>
+                        {this.renderToolbarSections()}
                     </div>
                 </nav>
 
