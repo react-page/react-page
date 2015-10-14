a1fe74f70ef87c50db0d1e53d8a1934b21f2aec0
Aeneas Rekkas <aeneas.rekkas@serlo.org>
2015-10-14 15:30:12 +0200

    unstaged

--- src/app/plugin/text/Component.js
+++ src/app/plugin/text/Component.js
@@ -1,21 +1,17 @@
 import React  from 'react';
 import map from 'lodash/collection/map';
 import cloneDeep from 'lodash/lang/cloneDeep';
-import Editor from 'medium-editor';
 import async from 'async';
 import HTMLParser from './HTMLParser';
 import isEmpty from 'lodash/lang/isEmpty';
 import Actions from 'app/actions/Partition';
 import find from 'lodash/collection/find';
 
-import {ProseMirror} from "prosemirror/dist/edit"
-import "prosemirror/dist/inputrules/autoinput"
-import "prosemirror/dist/menu/inlinemenu"
-import "prosemirror/dist/menu/buttonmenu"
-import "prosemirror/dist/menu/menubar"
-
-import './css/medium-editor.css';
-import './css/themes/default.css';
+import {ProseMirror} from "prosemirror/dist/edit";
+import "prosemirror/dist/inputrules/autoinput";
+import "prosemirror/dist/menu/inlinemenu";
+import "prosemirror/dist/menu/buttonmenu";
+import "prosemirror/dist/menu/menubar";
 
 function unwrap(elements) {
     async.each(elements, (el) => {
@@ -35,18 +31,20 @@ function unwrap(elements) {
 export default class Component extends React.Component {
     componentWillMount() {
         this.setState({
-            inner: map(this.props.sections, (s) => {
+            data: map(this.props.sections, (s) => {
                 var attrs = map(s.attrs, (v, k) => (k + '="' + v + '"')).join(' ') || '',
-                    tag = (s.data.tag || 'p'), elem = (tag + ' ' + attrs).trim();
-                return '<' + elem + ' data-id' + '="' + s.id + '">' + s.data.inner + '</' + tag + '>';
-            }).join('')
+                    tag = (s.data.tag || 'p'),
+                    elem = (tag + ' ' + attrs).trim();
+                return '<' + elem + '>' + s.data.inner + '</' + tag + '>';
+            }).join(''),
+            type: this.props.type || 'html'
         });
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        var e = React.findDOMNode(this.refs.area);
-        return e.innerHTML !== nextState.inner;
-    }
+    //shouldComponentUpdate(nextProps, nextState) {
+    //    var e = React.findDOMNode(this.refs.area);
+    //    return e.innerHTML !== nextState.inner;
+    //}
 
     componentDidMount() {
         var editable = React.findDOMNode(this.refs.area),
@@ -56,38 +54,40 @@ export default class Component extends React.Component {
                 inlineMenu: true,
                 buttonMenu: true,
                 autoInput: true,
-                doc: this.state.inner,
-                docFormat: "html"
+                doc: this.state.data,
+                docFormat: this.state.type
             });
         this.setState({editor: editor});
+
+        editor.on("change", () => {
+            console.log(editor.getContent("json"));
+        });
 
         //var editor = new Editor(editable, {toolbar: {buttons: ['bold', 'italic', 'anchor', 'h2', 'h3', 'quote']}});
         //this.setState({editor: editor});
         //editor.subscribe('editableInput', (event, editable) => {
 
-            // TODO FIXME upstream https://github.com/yabwe/medium-editor/issues/543
-            //unwrap(editable.querySelectorAll(':scope [style]'));
+        // TODO FIXME upstream https://github.com/yabwe/medium-editor/issues/543
+        //unwrap(editable.querySelectorAll(':scope [style]'));
 
-            // Sanitize the current input
-            //let sections = HTMLParser.parse(editable.querySelectorAll(':scope > *'), 'placeholder');
+        // Sanitize the current input
+        //let sections = HTMLParser.parse(editable.querySelectorAll(':scope > *'), 'placeholder');
 
-            // TODO FIXME this is a sort-of hack to prevent re-rendering the sections on every input change.
-            //let shouldUpdate = !isEmpty(find(sections, (s) => s.plugin === 'placeholder'));
-            //if (shouldUpdate) {
-            //    this.props.store.dispatch({
-            //        type: Actions.replace,
-            //        // FIXME make this configurable or something
-            //        with: sections
-            //    });
-            //}
+        // TODO FIXME this is a sort-of hack to prevent re-rendering the sections on every input change.
+        //let shouldUpdate = !isEmpty(find(sections, (s) => s.plugin === 'placeholder'));
+        //if (shouldUpdate) {
+        //    this.props.store.dispatch({
+        //        type: Actions.replace,
+        //        // FIXME make this configurable or something
+        //        with: sections
+        //    });
+        //}
         //});
     }
 
     render() {
-        return (
-            /*jshint ignore:start */
-            <div ref="area"/>
-            /*jshint ignore:end */
-        );
+        /*jshint ignore:start */
+        return <div ref="area"/>;
+        /*jshint ignore:end */
     }
 }
