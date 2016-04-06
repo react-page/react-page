6267debd5f5a2de26fa976851006cb6aebb0671a
Jonas Keinholz <jonas@keinholz.com>
2016-04-06 12:50:11 +0200

    Fix e2e testing on travis

--- bin/nightwatch.js
+++ bin/nightwatch.js
@@ -1,8 +1,1 @@
-const childProcess = require('child_process')
-
-const env = Object.assign({}, process.env, { PORT: 4000 })
-const server = childProcess.spawn('npm', ['start'], env)
-
-process.on('exit', () => server.kill())
-
 require('nightwatch/bin/runner.js')
