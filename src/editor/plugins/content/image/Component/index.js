58195058c3f360356b9b0791c972f40112f1866d
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-08-23 14:00:25 +0200

    more flow

--- src/editor/plugins/content/image/Component/index.js
+++ src/editor/plugins/content/image/Component/index.js
@@ -1,9 +1,10 @@
+// @flow
 import React, { PropTypes } from 'react'
 import cssModules from 'react-css-modules'
 
 import styles from './index.scoped.css'
 
-const Image = ({ state: { src } }) => (
+const Image = ({ state: { src } }: { state: { src: string } }) => (
   <div styleName="box">
     {src ? <img styleName="image" src={src} /> : <div styleName="placeholder" />}
   </div>
--- src/editor/plugins/content/spacer/Component/index.js
+++ src/editor/plugins/content/spacer/Component/index.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component, PropTypes } from 'react'
 import cssModules from 'react-css-modules'
 import debounce from 'lodash.debounce'
@@ -6,25 +7,25 @@ import classNames from 'classnames'
 
 import styles from './index.scoped.css'
 
-const compute = ({ height }) => ({ height: height > 24 ? height : 24 })
+const compute = ({ height }: { height: number }) => ({ height: height > 24 ? height : 24 })
 
-const fire = debounce(({ state, onChange }) => onChange(state), 1000, { leading: false })
+const fire = debounce(({ state, onChange }: { state: Object, onChange(state: Object): void }) => onChange(state), 1000, { leading: false })
 
-const Solid = ({ height }) => <div style={{ height }} />
+const Solid = ({ height }: { height: number }) => <div style={{ height }} />
 
 Solid.propTypes = {
   height: PropTypes.number
 }
 
 class Spacer extends Component {
-  constructor(props) {
-    super(props)
+  state = {}
 
+  constructor(props: Object) {
+    super(props)
     this.state = compute(props.state)
-    this.onResize = this.onResize.bind(this)
   }
 
-  onResize(event, { size }) {
+  onResize = (event: Event, { size }: { size: { height: number, width: number } }) => {
     const { onChange } = this.props
     const state = compute(size)
     this.setState(state)
