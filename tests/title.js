efdfce15013018f7814e965f1cca4a9b97ec5770
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-08-24 13:05:31 +0200

    core: e2e test don't work with firefox 47 - closes #63

--- tests/title.js
+++ tests/title.js
@@ -2,9 +2,9 @@ module.exports = {
   'Editor has the correct title'(client) {
     client
       .url(client.launch_url)
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body', 3000)
       .assert.title('Ory.am Editor')
-      .assert.containsText('SOUTH BENFLEET')
+      .assert.containsText('body', 'SOUTH BENFLEET')
       .end()
   }
 }
