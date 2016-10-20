7a70e83181156774392109c8fd299da30fc83fc3
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-10-20 11:47:03 +0200

    examples/sps: give fa-icon unique name/icon

--- examples/single-page-site/plugins/fa-icon/index.js
+++ examples/single-page-site/plugins/fa-icon/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react'
 import Component from './Component'
-import Panorama from 'material-ui/svg-icons/image/panorama'
+import Panorama from 'material-ui/svg-icons/toggle/star'
 import { ContentPlugin } from 'src/editor/service/plugin/classes'
 
 export default class FaIcon extends ContentPlugin {
@@ -9,7 +9,7 @@ export default class FaIcon extends ContentPlugin {
   name = 'example/content/fa-icon'
   version = '0.0.1'
   icon = <Panorama />
-  text = 'Image'
+  text = 'Icon'
   inlineable = true
   onRemoveHotKey = () => Promise.reject()
 }
