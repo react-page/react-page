eb0e9e9a73779836aaee0d833793818cfaeb9a0b
Aeneas <arekkas@users.noreply.github.com>
2016-11-16 22:31:29 +0100

    plugins/slate: when slate is focused, undo and redo behave weird #257 (#258)

--- src/editor/reducer/editables/index.js
+++ src/editor/reducer/editables/index.js
@@ -1,28 +1,61 @@
 // @flow
-import { editable, rawEditableReducer } from 'src/editor/reducer/editable'
+import { editable } from 'src/editor/reducer/editable'
 import { UPDATE_EDITABLE } from 'src/editor/actions/editables'
 import type { Editable } from 'types/editable'
+import undoable, { includeAction } from 'redux-undo'
+import { CELL_UPDATE_CONTENT, CELL_UPDATE_LAYOUT, CELL_REMOVE, CELL_RESIZE } from 'src/editor/actions/cell/core'
+import {
+  CELL_INSERT_ABOVE,
+  CELL_INSERT_BELOW,
+  CELL_INSERT_LEFT_OF,
+  CELL_INSERT_RIGHT_OF,
+  CELL_INSERT_INLINE_LEFT,
+  CELL_INSERT_INLINE_RIGHT
+} from 'src/editor/actions/cell/insert'
+import { set } from 'redux-undo/lib/debug'
 
-const createHistory = (state: any) => ({
-  past: [],
-  present: state,
-  future: []
-})
+set(true)
 
-export const editables = (state: { present: Editable }[] = [], action: {
+const inner = undoable((state: Array<Editable> = [], action: {
   type: string,
   id: string,
   editable: Editable
 }): Editable[] => {
   switch (action.type) {
-    case UPDATE_EDITABLE:
-      return [
-        ...state.filter(({ present: { id } }: { present: Editable }): boolean => id !== action.id),
-        // we need to run the rawreducer once or the history initial state will be inconsistent.
-        // resolves https://github.com/ory-am/editor/pull/117#issuecomment-242942796
-        createHistory(rawEditableReducer(action.editable, action))
-      ].map((e: { present: Editable }): Editable => editable(e.present.id)(e, action))
     default:
-      return state.map((e: { present: Editable }) => editable(e.present.id)(e, action))
+      return state.map((e: Editable) => editable(e, action))
+  }
+}, {
+  filter: includeAction([
+    CELL_UPDATE_CONTENT, CELL_UPDATE_LAYOUT, CELL_REMOVE, CELL_RESIZE,
+    CELL_INSERT_ABOVE, CELL_INSERT_BELOW, CELL_INSERT_LEFT_OF, CELL_INSERT_RIGHT_OF,
+    CELL_INSERT_INLINE_LEFT, CELL_INSERT_INLINE_RIGHT
+  ]),
+  // initTypes: [UPDATE_EDITABLE],
+  neverSkipReducer: true
+})
+
+export const editables = (state: { past: Editable[], present: Editable[], future: Editable[] } = { past: [], present: [], future: [] }, action: Object) => {
+  const { past = [], present = [], future = [] } = state
+  switch (action.type) {
+    case UPDATE_EDITABLE:
+      return inner({
+        past: past.map((editables: Editable[]) => [
+          ...editables.filter(({ id }: Editable): boolean => id !== action.id),
+          // we need to run the rawreducer once or the history initial state will be inconsistent.
+          // resolves https://github.com/ory-am/editor/pull/117#issuecomment-242942796
+          // ...past,
+          editable(action.editable, action)
+        ]),
+        present: inner([
+          ...present.filter(({ id }: Editable): boolean => id !== action.id),
+          // we need to run the rawreducer once or the history initial state will be inconsistent.
+          // resolves https://github.com/ory-am/editor/pull/117#issuecomment-242942796
+          editable(action.editable, action)
+        ]),
+        future
+      })
+    default:
+      return inner(state, action)
   }
 }
