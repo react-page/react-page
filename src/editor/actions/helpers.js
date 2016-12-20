b70a1039c3c1ea59aacf0ea5715ecde6d8a31aea
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2016-12-20 11:45:01 +0100

    vendor: node-uuid deprecated - closes #298

--- src/editor/actions/helpers.js
+++ src/editor/actions/helpers.js
@@ -1,10 +1,10 @@
 // @flow
-import uuid from 'node-uuid'
+import uuid from 'uuid/v4'
 
 export const gen = (c: number = 1) => {
   const ret = []
   for (let i = 0; i < c; i++) {
-    ret.push(uuid.v4())
+    ret.push(uuid())
   }
   return ret
 }
