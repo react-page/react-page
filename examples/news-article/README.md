717a8b9af2c227fab1aa198590c194b442469efe
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-01-20 11:18:21 +0100

    examples: improve example file structure

--- examples/news-article/README.md
+++
@@ -1,3 +0,0 @@
-# ORY Editor News Article Example
-
-This is a simple example app using the ORY Editor. It shows how the ORY Editor could be used to create a news article.
--- examples/src/demo/plugins/parallax/index.js
+++ examples/src/demo/plugins/parallax/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Announcement from 'material-ui/svg-icons/image/landscape'
-import Slate from 'src/editor/plugins/content/slate'
+import slate from 'ory-editor-plugins-slate'
 import uuid from 'uuid'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
@@ -8,15 +10,16 @@ import MenuItem from 'material-ui/MenuItem'
 type Props = {
   children: Node,
   readOnly: boolean,
-  state: {style: string },
+  state: { style: string },
   onChange(state: Object): void
 }
 
 const ParallaxComponent = ({ children, state: { style, align }, readOnly, onChange }: Props) => (
-  <section id="two" className={`main style${style}`}>
-    {
-      readOnly ? null : (
-        <div style={{
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <section id="two" className={`main style${style}`}>
+      {
+        readOnly ? null : (
+            <div style={{
           float: 'right',
           position: 'absolute',
           right: 0,
@@ -24,32 +27,33 @@ const ParallaxComponent = ({ children, state: { style, align }, readOnly, onChan
           backgroundColor: 'rgba(255,255,255,.8)',
           padding: '4px 20px'
         }}
-        >
-          <SelectField
-            value={style} onChange={(e: any, i: any, v: any) => onChange({ style: v })}
-            floatingLabelText="Select section style"
-          >
-            <MenuItem value={1} primaryText="White" />
-            <MenuItem value={2} primaryText="Mountains" />
-            <MenuItem value={3} primaryText="Lights" />
-            <MenuItem value={4} primaryText="Grass" />
-            <MenuItem value={5} primaryText="Lake" />
-          </SelectField>
-        </div>
-      )
-    }
-    <div className="container" style={{
+            >
+              <SelectField
+                value={style} onChange={(e: any, i: any, v: any) => onChange({ style: v })}
+                floatingLabelText="Select section style"
+              >
+                <MenuItem value={1} primaryText="White"/>
+                <MenuItem value={2} primaryText="Mountains"/>
+                <MenuItem value={3} primaryText="Lights"/>
+                <MenuItem value={4} primaryText="Grass"/>
+                <MenuItem value={5} primaryText="Lake"/>
+              </SelectField>
+            </div>
+          )
+      }
+      <div className="container" style={{
       textAlign: align
     }}
-    >
-      <div>
-        {children}
+      >
+        <div>
+          {children}
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </MuiThemeProvider>
 )
 
-const defaultPlugin = new Slate()
+const defaultPlugin = slate()
 
 export default {
   Component: ParallaxComponent,
