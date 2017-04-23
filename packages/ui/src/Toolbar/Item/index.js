9c37cd0be945c7798f49170ef76928169280a8d0
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-04-23 17:46:53 +0200

    ui: jump back to insert mode instead of add mode

--- packages/ui/src/Toolbar/Item/index.js
+++ packages/ui/src/Toolbar/Item/index.js
@@ -5,6 +5,7 @@ import draggable from '../Draggable'
 import ListItem from 'material-ui/List/ListItem'
 import { Plugin } from 'ory-editor-core'
 import DragHandle from 'material-ui/svg-icons/editor/drag-handle'
+import IconButton from 'material-ui/IconButton';
 
 // import logger from 'ory-editor-core/lib/service/logger'
 
@@ -22,8 +23,15 @@ const Item = ({ plugin, insert }: { plugin: Plugin, insert: any }, k: string) =>
       <ListItem
         leftAvatar={<Avatar icon={plugin.IconComponent} />}
         primaryText={plugin.text}
+        secondaryText={plugin.description}
+        secondaryTextLines={2}
         disabled
-        rightIcon={<DragHandle className="ory-toolbar-item-drag-handle" style={{ cursor: 'move' }} />}
+        className="ory-toolbar-item"
+        rightIcon={(
+          <IconButton tooltip="Click and hold to drag" tooltipPosition="bottom-left">
+            <DragHandle className="ory-toolbar-item-drag-handle" style={{ cursor: 'move' }} />
+          </IconButton>
+        )}
       />
     </Draggable>
   )
