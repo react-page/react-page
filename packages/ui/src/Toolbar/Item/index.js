781f35cc8e61923bbd9e01cc27ce499be656bff8
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-05-08 15:15:17 +0200

    ui/toolbar: resolve firefox issue with drag detection - closes #362

--- packages/ui/src/Toolbar/Item/index.js
+++ packages/ui/src/Toolbar/Item/index.js
@@ -1,42 +1,62 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import Avatar from 'material-ui/Avatar'
 import draggable from '../Draggable'
 import ListItem from 'material-ui/List/ListItem'
 import { Plugin } from 'ory-editor-core'
 import DragHandle from 'material-ui/svg-icons/editor/drag-handle'
-import IconButton from 'material-ui/IconButton';
+import Tooltip from 'rc-tooltip'
 
-// import logger from 'ory-editor-core/lib/service/logger'
+class Item extends Component {
+  props: { plugin: Plugin, insert: any }
+  state = { tooltipVisible: false }
 
-const Item = ({ plugin, insert }: { plugin: Plugin, insert: any }, k: string) => {
-  if (!plugin.IconComponent && !plugin.text) {
-    // logger.warn('Plugin text or plugin icon missing', plugin)
-    return null
+  onMouseEnter = () => {
+    this.setState({ tooltipVisible: true })
   }
 
-  const Draggable = draggable(plugin.name)
+  onMouseLeave = () => {
+    this.setState({ tooltipVisible: false })
+  }
 
-  // not using css modules here because they don't work with svg icons
-  return (
-    <ListItem
-      leftAvatar={<Avatar icon={plugin.IconComponent} />}
-      primaryText={plugin.text}
-      secondaryText={plugin.description}
-      secondaryTextLines={2}
-      disabled
-      className="ory-toolbar-item"
-      rightIconButton={(
-        <IconButton className="ory-toolbar-item-drag-handle-button"
-          tooltip="Click and hold to drag"
-          tooltipPosition="bottom-left">
+  render() {
+    const { plugin, insert, key: k } = this.props
+    if (!plugin.IconComponent && !plugin.text) {
+      // logger.warn('Plugin text or plugin icon missing', plugin)
+      return null
+    }
+
+    const Draggable = draggable(plugin.name)
+
+    // not using css modules here because they don't work with svg icons
+    return (
+      <ListItem
+        leftAvatar={<Avatar icon={plugin.IconComponent} />}
+        primaryText={plugin.text}
+        secondaryText={plugin.description}
+        secondaryTextLines={2}
+        disabled
+        className="ory-toolbar-item"
+        rightIcon={(
+          <span
+            className="ory-toolbar-item-drag-handle-button"
+            onMouseEnter={this.onMouseEnter}
+            onMouseLeave={this.onMouseLeave}
+            onMouseDown={this.onMouseLeave}
+          >
           <Draggable key={k} insert={insert}>
-            <DragHandle className="ory-toolbar-item-drag-handle" />
+            <Tooltip
+              visible={this.state.tooltipVisible}
+              placement="bottomLeft"
+              overlay={<span>Drag me!</span>}>
+              <DragHandle className="ory-toolbar-item-drag-handle" />
+            </Tooltip>
           </Draggable>
-        </IconButton>
-      )}
-    />
-  )
+        </span>
+        )}
+      />
+    )
+  }
 }
 
 export default Item
