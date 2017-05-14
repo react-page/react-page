95a58c7e145a6cd877f838368467b604ba1138dd
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-05-14 16:53:24 +0200

    ci: improve ci chain

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-if [ "$TEST_TYPE" == "build" ]; then
-  npm run build:lib
-  cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
-  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-  lerna publish --skip-git --repo-version=$(git describe --tag) --yes --force-publish=*
-fi
+npm run build:lib
+cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+lerna publish --skip-git --repo-version=$(git describe --tag) --yes --force-publish=*
