9105604618f971890cb11e37e0f20669b9c7a770
Aeneas Rekkas (arekkas) <aeneas@ory.am>
2017-05-14 13:51:39 +0200

    core: update content when updating plugins
    Closes: #368

--- packages/core/src/service/plugin/missing.js
+++ packages/core/src/service/plugin/missing.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import type { ContentPluginProps } from './classes'
 
-const Missing = (props: ContentPluginProps<{}>) => (
+const ContentMissingComponent = (props: ContentPluginProps<{}>) => (
   <div
     style={{
       backgroundColor: 'red',
@@ -12,12 +12,36 @@ const Missing = (props: ContentPluginProps<{}>) => (
       overflowX: 'scroll'
     }}
   >
-    The requested plugin could not be found.
+    The requested content plugin could not be found.
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 )
 
-export default (plugin: { name: string, version: string }) => ({
-  Component: Missing,
+export const contentMissing = (plugin: { name: string, version: string }) => ({
+  Component: ContentMissingComponent,
   ...plugin
 })
+
+const LayoutMissingComponent = ({ children, ...props }: any) => (
+  <div>
+    <div
+      style={{
+        backgroundColor: 'red',
+        padding: '8px',
+        border: '1px solid black',
+        margin: '2px',
+        overflowX: 'scroll'
+      }}
+    >
+      The requested layout plugin could not be found.
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
+    {children}
+  </div>
+)
+
+export const layoutMissing = (plugin: { name: string, version: string }) => ({
+  Component: LayoutMissingComponent,
+  ...plugin
+})
+
