faefea643c02b1010bf755d5b9f047fc3f3e0eb1
Ben Tillman <ben.tillman@gmail.com>
2018-06-06 23:21:08 +1200

    Upgrade image plugin to use material-ui 1.2.0

--- packages/plugins/content/image/src/Component/index.js
+++ packages/plugins/content/image/src/Component/index.js
@@ -5,12 +5,12 @@
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ORY Editor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ORY Editor.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -25,13 +25,12 @@ import React from 'react'
 import Display from './Display'
 import Form from './Form'
 import type { ContentPluginProps } from 'ory-editor-core/lib/service/plugin/classes'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 export type PropTypes = ContentPluginProps<{ src: string, caption: string }>
 
 const Image = (props: PropTypes) => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
+  <MuiThemeProvider theme={createMuiTheme()}>
     {props.readOnly ? <Display {...props} /> : <Form {...props} />}
   </MuiThemeProvider>
 )
