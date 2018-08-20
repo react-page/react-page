e423b848d747d17928f9d45d9a5d45c50202f490
arekkas <aeneas@ory.am>
2018-08-20 17:08:45 +0200

    Bump flow-bin and update typdefs
    Signed-off-by: arekkas <aeneas@ory.am>

--- flow-typed/npm/flow-bin_v0.x.x.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: 67b0c3a16b2d6f8ef0a31a5745a0b3e1
-// flow-typed version: 3817bc6980/flow-bin_v0.x.x/flow_>=v0.25.x
-
-declare module "flow-bin" {
-  declare module.exports: string;
-}
