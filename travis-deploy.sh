f0b69d791a02502d7ea794daa4e24bee8e2e0376
arekkas <aeneas@ory.am>
2018-08-25 11:50:33 +0200

    travis: Upgrade travis deployment script
    Closes #574
    
    Signed-off-by: arekkas <aeneas@ory.am>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 
 npm run build:lib
 cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
+git checkout -b release-$(git describe --tag)
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-lerna publish --skip-git --repo-version=$(git describe --tag) --yes --force-publish=*
+lerna publish --no-git-tag-version --no-push --yes --force-publish=* $(git describe --tag)
