0a1376b1e9dcbbc12a99c8a65fc5bf2f08521570
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2019-05-23 22:43:21 -0300

    Update travis-deploy.sh

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-npm run build:lib
 cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
 git checkout -b release-$(git describe --tag)
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
