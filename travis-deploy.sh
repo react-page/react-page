ef611a5ed93b2405d30dc764c4a2f1de6e7f42a2
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2019-06-04 14:56:21 -0300

    Set the npm version on package json

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,7 +6,6 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
 git checkout -b release-$(git describe --tag)
-npm i npm@^6
 cd ./packages/meta
 npm cache clean --force
 cd ../../
