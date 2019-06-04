76368da4d92fe48936ed4d5cf216953460304eb2
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2019-06-04 13:39:44 -0300

    Clean package/meta before publish

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+
 cat ./packages/meta/package.json | sed s/0.0.0/$(git describe --tag)/g  > ./packages/meta/package.json
 git checkout -b release-$(git describe --tag)
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-lerna publish --no-git-tag-version --no-push --yes --force-publish=* $(git describe --tag)
+cd ./packages/meta
+npm cache clean --force
+cd ../../
+yarn publish
