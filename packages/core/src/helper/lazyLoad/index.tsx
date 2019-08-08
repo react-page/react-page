bace4d2b5993107179517c88050e0dd67a7b7c3c
Marco <macrozone@gmail.com>
2019-08-08 16:17:25 +0200

    fix: use @loadable/component instead of react.lazy because of ssr issues

--- packages/core/src/helper/lazyLoad/index.tsx
+++ packages/core/src/helper/lazyLoad/index.tsx
@@ -1,11 +1,4 @@
-import * as React from 'react';
+import loadable from '@loadable/component';
 
-// we wrap the suspense directly atm
 // tslint:disable-next-line:no-any
-const withSuspense = (C: any) => (props: any) => (
-  <React.Suspense fallback={<div />}>
-    <C {...props} />
-  </React.Suspense>
-);
-// tslint:disable-next-line:no-any
-export default (loader: any) => withSuspense(React.lazy(loader));
+export default (loader: any) => loadable(loader);
