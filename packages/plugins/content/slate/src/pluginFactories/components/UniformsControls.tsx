bdac07a403a82e02c26df68a4f7864c44066dff7
Marco <macrozone@gmail.com>
2019-08-28 16:30:25 +0200

    fix: remove log

--- packages/plugins/content/slate/src/pluginFactories/components/UniformsControls.tsx
+++ packages/plugins/content/slate/src/pluginFactories/components/UniformsControls.tsx
@@ -1,27 +1,3 @@
-/*
- * This file is part of ORY Editor.
- *
- * ORY Editor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ORY Editor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with ORY Editor.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @license LGPL-3.0
- * @copyright 2016-2018 Aeneas Rekkas
- * @author Aeneas Rekkas <aeneas+oss@aeneas.io>
- *
- */
-
-/* eslint-disable no-alert, prefer-reflect, default-case, react/display-name */
-
 import React, { useState, useEffect, useCallback } from 'react';
 import TextField from '@material-ui/core/TextField';
 
@@ -41,7 +17,6 @@ function Controls<T>(props: SlatePluginControls<T>) {
     () => {
       // update data if reopens
       if (props.open) {
-        console.log(props.data);
         setData(props.data);
       }
     },
@@ -49,14 +24,12 @@ function Controls<T>(props: SlatePluginControls<T>) {
   );
 
   const onCancel = () => {
-    console.log('on cancel');
     props.close();
     props.editor.focus();
   };
 
   const onSubmit = useCallback(
     () => {
-      console.log('on submit', data);
       if (props.shouldInsertWithText) {
         props.addWithText(text, data);
       } else {
@@ -91,7 +64,6 @@ function Controls<T>(props: SlatePluginControls<T>) {
           autosave={true}
           schema={makeUniformsSchema<T>(props.schema)}
           onSubmit={m => {
-            console.log('autoform onSubmit', m);
             setData(m);
           }}
         >
