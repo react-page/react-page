04e8d161b19ad1a50ee3e22f0ce9ed8528170a30
Marco <macrozone@gmail.com>
2019-08-28 15:50:49 +0200

    fix case

--- packages/plugins/createPluginMaterialUi/src/index.tsx
+++ packages/plugins/createPluginMaterialUi/src/index.tsx
@@ -1,7 +1,7 @@
 import createContentPlugin from './createContentPlugin';
 import createLayoutPlugin from './createLayoutPlugin';
 import makeUniformsSchema from './utils/makeUniformsSchema';
-import { JsonSchema } from './types/JsonSchema';
+import { JsonSchema } from './types/jsonSchema';
 export {
   createContentPlugin,
   createLayoutPlugin,
