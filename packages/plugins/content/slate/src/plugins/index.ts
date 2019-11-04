1c7f35375d2b35af5df6e5c36778d601a82f4100
Marco <macrozone@gmail.com>
2019-11-04 17:31:46 +0100

    fix plugin imports

--- packages/plugins/content/slate/src/plugins/index.ts
+++ packages/plugins/content/slate/src/plugins/index.ts
@@ -1,18 +1,18 @@
 import alignment from './alignment';
-import blockquote from './quotes';
+import quotes from './quotes';
 import code from './code/';
 import emphasize from './emphasize';
 import headings from './headings';
 import link from './links';
 import lists from './lists';
-import paragraph from './paragraphs';
+import paragraphs from './paragraphs';
 export {
   alignment,
-  blockquote,
+  quotes,
   code,
   emphasize,
   headings,
   link,
   lists,
-  paragraph
+  paragraphs
 };
