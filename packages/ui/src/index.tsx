275a286b04ed514b69e5d50aaa23dfc751149e9c
Marco <macrozone@gmail.com>
2019-11-15 14:23:51 +0100

    fix: remove unused,
    fix: lazy load ui again

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,13 +1,13 @@
 // something is wrong with lerna, typescript and this import: import { lazyLoad } from '@react-page/core';
 import loadable from '@loadable/component';
 import { colorToString } from './ColorPicker/colorToString';
-import EditorUI from './EditorUI/index';
 import darkTheme from './ThemeProvider/DarkTheme';
 
 const Trash = loadable(() => import('./Trash/index'));
 const Toolbar = loadable(() => import('./Toolbar/index'));
 const DisplayModeToggle = loadable(() => import('./DisplayModeToggle/index'));
 const BottomToolbar = loadable(() => import('./BottomToolbar/index'));
+const EditorUI = loadable(() => import('./EditorUI/index'));
 
 const ThemeProvider = loadable(() => import('./ThemeProvider/index'));
 const ImageUpload = loadable(() => import('./ImageUpload/index'));
