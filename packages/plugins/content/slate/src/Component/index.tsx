26fdae18f5c738a74af77f5c3a0c1e59c1366c28
Marco Wettstein <maw@panter.ch>
2020-01-11 18:01:25 +0100

    readded renderer

--- packages/plugins/content/slate/src/Component/index.tsx
+++ packages/plugins/content/slate/src/Component/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 // tslint:disable-next-line:no-any
-export default (props: any) => {
+export default React.memo((props: any) => {
   const { Controls, Renderer, readOnly } = props;
   // slate controls currently contain everything
   return <>{!readOnly ? <Controls {...props} /> : <Renderer {...props} />}</>;
-};
+});
