cee9ba8f29ecd0c0d5d23513f7c404955059716a
Marco Wettstein <maw@panter.ch>
2020-01-19 19:38:34 +0100

    remove console log

--- packages/plugins/content/slate/src/index.tsx
+++ packages/plugins/content/slate/src/index.tsx
@@ -131,7 +131,6 @@ function plugin<TPlugins extends SlatePluginCollection = DefaultPlugins>(
 
     // tslint:disable-next-line:no-any
     reducer: (state: any, action: AnyAction) => {
-      console.log('reducer', state, action);
       if (
         (action.type === ActionTypes.UNDO ||
           action.type === ActionTypes.REDO) &&
