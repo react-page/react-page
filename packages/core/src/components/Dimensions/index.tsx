09a500cb1063bc89d3c2f46af41d50e0cd1e21af
Marco Wettstein <maw@panter.ch>
2020-02-07 12:56:08 +0100

    satisfy linter

--- packages/core/src/components/Dimensions/index.tsx
+++ packages/core/src/components/Dimensions/index.tsx
@@ -62,7 +62,6 @@ const Dimensions = ({
     }
 
     // This cann not be used here because it doesn't listen to state changes.
-  
 
     public componentWillUnmount() {
       this.getWindow().removeEventListener('resize', this.onResize);
