4b71281cefc3b74a89e4d3deedd003f0b1193f38
Marco Wettstein <maw@panter.ch>
2020-06-06 14:11:26 +0200

    fix: keep size on optimize in certain cases

--- packages/core/src/reducer/editable/helper/optimize.ts
+++ packages/core/src/reducer/editable/helper/optimize.ts
@@ -33,38 +33,43 @@ export const optimizeCells = (cells: Array<Cell> = []): Array<Cell> =>
 export const optimizeRows = (rows: Array<Row> = []): Array<Row> =>
   rows.filter(emptyFilter);
 
-export const optimizeCell = ({ rows, ...other }: Cell): Cell => ({
-  ...other,
-  rows: (rows || [])
-    .map(
-      (r: Row): Array<Row> => {
-        const { cells = [] } = r;
-        if (cells.length !== 1) {
+export const optimizeCell = (cell: Cell): Cell => {
+  const { rows, ...other } = cell;
+  const optimized = {
+    ...other,
+    rows: (rows || [])
+      .map(
+        (r: Row): Array<Row> => {
+          const { cells = [] } = r;
+          if (cells.length !== 1) {
+            return [r];
+          }
+
+          const { rows: cellRows = [], layout }: Cell = cells[0];
+          if (cellRows.length > 0 && !layout) {
+            return cellRows;
+          }
           return [r];
         }
+      )
+      .reduce(flatten, []),
+  };
 
-        const { rows: cellRows = [], layout }: Cell = cells[0];
-        if (cellRows.length > 0 && !layout) {
-          return cellRows;
-        }
-        return [r];
-      }
-    )
-    .reduce(flatten, []),
-});
+  return optimized;
+};
 
 export const optimizeRow = ({ cells, ...other }: Row): Row => ({
   ...other,
   cells: (cells || [])
     .map((c: Cell) => {
-      const { rows = [] } = c;
+      const { rows = [], size } = c;
       if (rows.length !== 1 || c.layout) {
         return [c];
       }
 
       const { cells: rowCells = [] }: Row = rows[0];
       if (rowCells.length === 1) {
-        return rowCells;
+        return rowCells.map((r) => ({ ...r, size }));
       }
 
       return [c];
