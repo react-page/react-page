1cda08588ab51706985682d471bba029ad428c1d
Marco Wettstein <maw@panter.ch>
2020-10-13 00:27:07 +0200

    feat: add version number to display

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -2,3 +2,5 @@ import { EditableType } from '@react-page/core';
 import editor, { EditorProps } from './editor';
 export { EditableType, EditorProps };
 export default editor;
+
+export const VERSION = '###VERSION###';
