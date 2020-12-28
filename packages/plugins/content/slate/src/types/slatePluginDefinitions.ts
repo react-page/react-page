e0d719d25c919cf6e65a80f6c8542dcbb1173245
Marco Wettstein <maw@panter.ch>
2020-12-28 22:17:21 +0100

    feat: more js docs

--- packages/plugins/content/slate/src/types/slatePluginDefinitions.ts
+++ packages/plugins/content/slate/src/types/slatePluginDefinitions.ts
@@ -27,19 +27,45 @@ export type SlatePluginControls<T extends {}> = {
 } & PluginButtonProps;
 
 export type SlateBasePluginDefinition<T extends {}> = {
+  /** define a hotkey to toggle this plugin **/
   hotKey?: string;
+
+  /**
+   * the schema defines the controls of the plugin
+   */
+  schema?: JsonSchema<T>;
+  /**
+   *
+   */
+  Controls?: React.ComponentType<SlatePluginControls<T>>;
+  /**
+   * icon of this plugin in the toolbar
+   */
+  icon?: JSX.Element;
+  /**
+   * label / title of this plugin
+   */
+  label?: string;
+  /**
+   * whether to show this plugin the hover toolbar
+   */
+  addHoverButton: boolean;
+  /**
+   * whether to show this plugin in the bottom toolbar
+   */
+  addToolbarButton: boolean;
+  /**
+   * defines the initial data when an element is added
+   */
+  getInitialData?: () => T;
+
+  isDisabled?: (editor: ReactEditor) => boolean;
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onKeyDown?: (e: React.KeyboardEvent, editor: ReactEditor, next: any) => void;
-  schema?: JsonSchema<T>;
-  Controls?: React.ComponentType<SlatePluginControls<T>>;
-  icon?: JSX.Element;
-  label?: string;
-  addHoverButton: boolean;
-  addToolbarButton: boolean;
+
   customAdd?: (editor: ReactEditor) => void;
   customRemove?: (editor: ReactEditor) => void;
-  isDisabled?: (editor: ReactEditor) => boolean;
-  getInitialData?: () => T;
 };
 
 export type SlateNodeBasePluginDefinition<T extends {}> = {
@@ -84,24 +110,67 @@ type NoInfer<T> = [T][T extends any ? 0 : never];
 export type SlateComponentPluginDefinition<
   T extends {}
 > = SlateNodeBasePluginDefinition<T> & {
+  /**
+   * a unique type (id) for the this component
+   */
   type: string;
-  getStyle?: (s: T) => React.CSSProperties;
+  /**
+   * pass a function that receives the data and returns the style that is passed to the Component
+   */
+  getStyle?: (data: T) => React.CSSProperties;
+  /**
+   * defines how this element can be created by pasted html
+   */
   deserialize?: {
+    /**
+     * html tag name
+     */
     tagName: string;
+    /**
+     * pass a function that receives the html element and returns data found in that element
+     */
     getData?: (el: HTMLElement) => T;
   };
+  /**
+   * the Component that renders this element. Can be a primitiv component like "div", "p", etc.
+   * or a complex Component. If its a complex component, you should render the children passed in it
+   *
+   */
   Component:
     | keyof JSX.IntrinsicElements
     | React.ComponentType<
         NoInfer<
           {
+            /**
+             * the attributes should be passed directly to the rendered html element
+             */
             attributes?: object;
+            /**
+             * the style that can be passed directly to the rendered html element
+             */
             style?: React.CSSProperties;
+            /**
+             * className to pass to the renderd html element
+             */
             className?: string;
+            /**
+             * raw child nodes. Usefull in certain niche cases
+             */
             childNodes: Node[];
-            getTextContents: () => string[];
+
+            /**
+             * hook that returns true if the current element is focused
+             */
             useFocused: () => boolean;
+            /**
+             * hook that returns true if the current element is selected
+             */
             useSelected: () => boolean;
+
+            /**
+             * @returns the current text content as an array. Usefull in some advanced use cases
+             */
+            getTextContents: () => string[];
           } & T
         >
       >;
