2ad8f45d192828c385435c362e5528ecb0592e2a
Marco Wettstein <maw@panter.ch>
2021-04-16 12:09:58 +0200

    fix: switching between resize mode and other modes is slow
    fixes https://github.com/react-page/react-page/issues/962

--- packages/editor/src/core/components/Cell/Draggable/index.tsx
+++ packages/editor/src/core/components/Cell/Draggable/index.tsx
@@ -4,7 +4,6 @@ import {
   useCell,
   useFocusCell,
   useIsLayoutMode,
-  useIsResizeMode,
   useOptions,
 } from '../../hooks';
 import { useDragHandle } from './useDragHandle';
@@ -25,55 +24,11 @@ const Draggable: React.FC<Props> = ({ isLeaf, children, nodeId }) => {
   const [isDragging, dragRef, previewElement] = useDragHandle(nodeId);
 
   const focus = useFocusCell(nodeId);
-
   const isLayoutMode = useIsLayoutMode();
-  const isResizeMode = useIsResizeMode();
   const options = useOptions();
-  if (isResizeMode) {
-    return <>{children}</>;
-  }
-  if (!isLayoutMode && !options.allowMoveInEditMode) {
-    return (
-      <div
-        className="react-page-cell-draggable-container"
-        style={{
-          height: '100%',
-        }}
-      >
-        {/* these divs are here to prevent page jumping, they are a placeholder for draggable / draggable-overlay */}
-        <div className="react-page-cell-draggable-overlay-placeholder" />
-        {children}
-      </div>
-    );
-  }
+
   const ResizeHandle =
     options.components?.EditModeResizeHandle ?? DefaultSmallHandle;
-
-  if (options.allowMoveInEditMode && !isLayoutMode) {
-    return (
-      <>
-        {previewElement}
-        <div
-          style={{
-            height: '100%',
-          }}
-          className={classNames({
-            'react-page-cell-draggable-in-edit': options.allowMoveInEditMode,
-            'react-page-cell-draggable':
-              isLayoutMode && !options.allowMoveInEditMode,
-            'react-page-cell-draggable-is-dragging': isDragging,
-          })}
-          onMouseDown={(e) => e.stopPropagation()}
-        >
-          <div ref={dragRef}>
-            <ResizeHandle onClick={focus} />
-          </div>
-
-          {children}
-        </div>
-      </>
-    );
-  }
 
   return (
     <>
@@ -82,19 +37,29 @@ const Draggable: React.FC<Props> = ({ isLeaf, children, nodeId }) => {
         style={{
           height: '100%',
         }}
-        ref={dragRef}
         className={classNames({
-          'react-page-cell-draggable': isLayoutMode,
+          'react-page-cell-draggable-in-edit': options.allowMoveInEditMode,
+          'react-page-cell-draggable':
+            isLayoutMode && !options.allowMoveInEditMode,
           'react-page-cell-draggable-is-dragging': isDragging,
         })}
+        onMouseDown={(e) => e.stopPropagation()}
       >
-        <div
-          className={classNames({
-            'react-page-cell-draggable-overlay': isLayoutMode,
-            [`react-page-cell-draggable-inline-${cell.inline}`]: cell.inline,
-            'react-page-cell-draggable-leaf': isLeaf,
-          })}
-        ></div>
+        {isLayoutMode ? (
+          <div
+            ref={dragRef}
+            className={classNames({
+              'react-page-cell-draggable-overlay': isLayoutMode,
+              [`react-page-cell-draggable-inline-${cell.inline}`]: cell.inline,
+              'react-page-cell-draggable-leaf': isLeaf,
+            })}
+          ></div>
+        ) : options.allowMoveInEditMode ? (
+          <div ref={dragRef}>
+            <ResizeHandle onClick={focus} />
+          </div>
+        ) : null}
+
         {children}
       </div>
     </>
