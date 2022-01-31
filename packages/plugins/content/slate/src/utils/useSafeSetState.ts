a1a83edfd65808f9e6095595e221e88faaf54ccf
neutron <hichemchouaibi2@gmail.com>
2022-01-31 10:58:42 -0500

    update lint

--- packages/plugins/content/slate/src/utils/useSafeSetState.ts
+++ packages/plugins/content/slate/src/utils/useSafeSetState.ts
@@ -1,25 +1,25 @@
 import * as React from 'react';
 
 export function useSafeSetState<T>(
-    initialState?: T | (() => T)
+  initialState?: T | (() => T)
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-    const [state, setState] = React.useState(initialState);
+  const [state, setState] = React.useState(initialState);
 
-    const mountedRef = React.useRef(false);
-    React.useEffect(() => {
-        mountedRef.current = true;
-        return () => {
-            mountedRef.current = false;
-        };
-    }, []);
-    const safeSetState = React.useCallback(
-        args => {
-            if (mountedRef.current) {
-                return setState(args);
-            }
-        },
-        [mountedRef, setState]
-    );
+  const mountedRef = React.useRef(false);
+  React.useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+  const safeSetState = React.useCallback(
+    (args) => {
+      if (mountedRef.current) {
+        return setState(args);
+      }
+    },
+    [mountedRef, setState]
+  );
 
-    return [state, safeSetState];
+  return [state, safeSetState];
 }
