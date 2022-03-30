4464e6d802c13e8991905130039f30a4ee34b064
Marco Wettstein <maw@panter.ch>
2022-03-30 10:45:34 +0200

    wip: feat: support react 18

--- examples/next.config.js
+++ examples/next.config.js
@@ -20,4 +20,13 @@ module.exports = withBundleAnalyzer({
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  // workaround for react18 and https://github.com/react-dnd/react-dnd/issues/3416
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react/jsx-runtime.js': 'react/jsx-runtime',
+      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
+    };
+    return config;
+  },
 });
