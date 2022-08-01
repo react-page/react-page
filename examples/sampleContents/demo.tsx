04eb3b811fdf3134a075f1c12d53b631a20cb64a
Gerom76 <gerom76@gmail.com>
2022-08-01 20:50:12 +0200

    Restored demo sample

--- examples/sampleContents/demo.tsx
+++ examples/sampleContents/demo.tsx
@@ -4,6 +4,35 @@ export const demo: Value = {
   version: 1,
   rows: [
     {
+      id: '4c7d90',
+      cells: [
+        {
+          id: '95d678',
+          size: 12,
+          plugin: { id: 'ory/editor/core/content/slate', version: 1 },
+          dataI18n: {
+            en: {
+              slate: [
+                {
+                  children: [{ text: 'Next Level Content Editing' }],
+                  type: 'HEADINGS/HEADING-TWO',
+                  data: { align: 'center' },
+                },
+                {
+                  children: [{ text: 'ReactPage' }],
+                  type: 'HEADINGS/HEADING-ONE',
+                  data: { align: 'center' },
+                },
+              ],
+            },
+          },
+          rows: [],
+          inline: null,
+        },
+      ],
+    },
+
+    {
       id: 'loa2uC',
       cells: [
         {
