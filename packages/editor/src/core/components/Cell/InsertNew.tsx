ed641011ea17b0ed3823775b03b9208c5cf82e97
Sebastian <sebastian@squidex.io>
2022-11-29 15:59:54 +0100

    Fix insert button.

--- packages/editor/src/core/components/Cell/InsertNew.tsx
+++ packages/editor/src/core/components/Cell/InsertNew.tsx
@@ -68,7 +68,18 @@ const InsertNew: React.FC<InsertNewProps> = ({ parentCellId }) => {
         setReferenceNodeId(parentCellId);
         setInsertMode();
       }}
-    />
+    >
+      <div className="react-page-cell-insert-new-icon">
+        <svg
+          focusable="false"
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          data-testid="AddIcon"
+        >
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+        </svg>
+      </div>
+    </div>
   );
 };
 
