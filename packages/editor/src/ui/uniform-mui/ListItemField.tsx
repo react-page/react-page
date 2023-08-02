8ccaa1919b7a13a0e22677785ac6660afdfed09a
Hussam Albasha <hussam.dev.001@gmail.com>
2023-08-02 15:59:56 +0300

    fixed build bug

--- packages/editor/src/ui/uniform-mui/ListItemField.tsx
+++ packages/editor/src/ui/uniform-mui/ListItemField.tsx
@@ -1,7 +1,131 @@
+// import type { ReactNode } from 'react';
+// import React from 'react';
+
+// import { Divider } from '@mui/material';
+// import type { ListItemProps } from '@mui/material/ListItem';
+// import ListItemMaterial from '@mui/material/ListItem';
+// import Stack from '@mui/material/Stack';
+// import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+
+// import { useDrag, useDrop } from 'react-dnd';
+// import { connectField, joinName, useField } from 'uniforms';
+
+// import AutoField from './AutoField';
+// import ListDelField from './ListDelField';
+// import ListSortField from './ListSortField';
+
+// export enum DragItemType {
+//   ListItemField = 'ListItemField',
+// }
+
+// interface DragItem {
+//   name: string;
+//   originalIndex: number;
+// }
+
+// export type ListItemFieldProps = {
+//   children?: ReactNode;
+//   dense?: ListItemProps['dense'];
+//   disableGutters?: ListItemProps['disableGutters'];
+//   divider?: ListItemProps['divider'];
+//   removeIcon?: ReactNode;
+//   dragIcon?: ReactNode;
+//   moveItemUpIcon?: ReactNode;
+//   moveItemDownIcon?: ReactNode;
+//   disableSortable?: boolean;
+//   value?: unknown;
+//   name?: string;
+// };
+
+// function ListItem({
+//   children = <AutoField label={null} name="" />,
+//   dense = true,
+//   disableGutters,
+//   divider,
+//   removeIcon,
+//   moveItemUpIcon,
+//   moveItemDownIcon,
+//   dragIcon = <DragIndicatorIcon />,
+//   disableSortable,
+//   value,
+//   name,
+// }: ListItemFieldProps) {
+//   const nameParts = joinName(null, name);
+//   const nameIndex = +nameParts[nameParts.length - 1];
+//   const parentName = joinName(nameParts.slice(0, -1));
+
+//   const parent = useField<Record<string, unknown>, unknown[]>(
+//     parentName,
+//     {},
+//     { absoluteName: true }
+//   )[0];
+
+//   const moveItem = (fromIndex: number, toIndex: number) => {
+//     const value = (parent.value ?? []).slice();
+//     value.splice(fromIndex, 1);
+//     value.splice(toIndex, 0, (parent.value ?? [])[fromIndex]);
+//     parent.onChange(value);
+//   };
+
+//   const [, drag] = useDrag<DragItem, unknown>(
+//     () => ({
+//       type: DragItemType.ListItemField,
+//       item: { name, originalIndex: nameIndex } as DragItem,
+//     }),
+//     [value, nameIndex, moveItem]
+//   );
+
+//   const [, drop] = useDrop(
+//     () => ({
+//       accept: DragItemType.ListItemField,
+//       drop: (draggedItem: DragItem, monitor) => {
+//         const didDrop = monitor.canDrop();
+//         if (didDrop && draggedItem.name !== name)
+//           moveItem(draggedItem.originalIndex, nameIndex);
+//       },
+//     }),
+//     [moveItem]
+//   );
+
+//   const disableSort = disableSortable ?? (parent.value ?? []).length < 2;
+
+//   return (
+//     <ListItemMaterial
+//       dense={dense}
+//       disableGutters={disableGutters}
+//       divider={divider}
+//       sx={{ gap: '0.5rem', p: 0, m: 0 }}
+//       ref={(node: DragItem) => (disableSort ? null : drag(drop(node)))}
+//     >
+// <Divider
+//   sx={{
+//     margin: '20px 0',
+//   }}
+// />
+// <Stack direction="row" justifyContent="space-between">
+//   <ListSortField
+//     name=""
+//     iconUp={moveItemUpIcon}
+//     iconDown={moveItemDownIcon}
+//     handleMove={moveItem}
+//     dragIcon={dragIcon}
+//     disabled={disableSort}
+//   />
+//   <ListDelField name="" icon={removeIcon} />
+// </Stack>
+// {children}
+//     </ListItemMaterial>
+//   );
+// }
+
+// export default connectField<ListItemFieldProps>(ListItem, {
+//   initialValue: false,
+// });
+
 import type { ReactNode } from 'react';
 import React from 'react';
 
-import { Divider } from '@mui/material';
+import { Divider, IconButton } from '@mui/material';
 import type { ListItemProps } from '@mui/material/ListItem';
 import ListItemMaterial from '@mui/material/ListItem';
 import Stack from '@mui/material/Stack';
@@ -90,17 +214,19 @@ function ListItem({
   const disableSort = disableSortable ?? (parent.value ?? []).length < 2;
 
   return (
-    <>
+    <ListItemMaterial
+      dense={dense}
+      disableGutters={disableGutters}
+      divider={divider}
+      ref={(node) => (disableSort ? null : drag(drop(node)))}
+      sx={{ gap: '0.5rem', display: 'block' }}
+    >
       <Divider
         sx={{
           margin: '20px 0',
         }}
       />
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        ref={(node: DragItem) => (disableSort ? null : drag(drop(node)))}
-      >
+      <Stack direction="row" justifyContent="space-between">
         <ListSortField
           name=""
           iconUp={moveItemUpIcon}
@@ -111,15 +237,8 @@ function ListItem({
         />
         <ListDelField name="" icon={removeIcon} />
       </Stack>
-      <ListItemMaterial
-        dense={dense}
-        disableGutters={disableGutters}
-        divider={divider}
-        sx={{ gap: '0.5rem', p: 0, m: 0 }}
-      >
-        {children}
-      </ListItemMaterial>
-    </>
+      {children}
+    </ListItemMaterial>
   );
 }
 
